0b76370cf7cb5ad02ba27cafc4d152892aa5cde2
Alessia Masola <2202343m@student.gla.ac.uk>
2018-03-24 16:13:35 -0700

    trying to add a sliding menu

--- HomeSafe/ViewController.swift
+++ HomeSafe/ViewController.swift
@@ -11,22 +11,80 @@ import GoogleMaps
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewConstraint: NSLayoutConstraint!
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    @IBOutlet weak var sideView: UIView!
+    
+    //self.view.bringSubviewToFront(self.sideView)
+    
+    
     override func viewDidLoad() {
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        super.viewDidLoad()
+        blurView.layer.cornerRadius = 15
+        sideView.layer.shadowColor = UIColor.black.cgColor
+        sideView.layer.shadowOpacity = 0.8
+        sideView.layer.shadowOffset = CGSize(width: 5, height:0)
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 37.787569, longitude: -122.396484, zoom: 12.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
+        mapView.camera = camera
+        mapView.bringSubview(toFront: sideView)
+        mapView.bringSubview(toFront: blurView)
+        //mapView.bringSubview(toFront: viewConstraint)
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2D(latitude: 37.787569, longitude: -122.396484)
+        marker.title = "San Francisco"
+        marker.snippet = "California"
         marker.map = mapView
-    
+        
+        
     }
-
+    @IBAction func panPerformed(_ sender: UIPanGestureRecognizer) {
+        
+        if sender.state == .began || sender.state == .changed {
+            let translation = sender.translation(in: self.view).x
+            if translation > 0 { // swipe right
+                if viewConstraint.constant < 20 {
+                    UIView.animate(withDuration: 0.2, animations: {
+                        self.viewConstraint.constant += translation / 10
+                        self.view.layoutIfNeeded()
+                    })
+                }
+            } else { // swipe left
+                if viewConstraint.constant > -175 {
+                    UIView.animate(withDuration: 0.2, animations: {
+                        self.viewConstraint.constant += translation / 10
+                        self.view.layoutIfNeeded()
+                    })
+                }
+            }
+            
+        } else if sender.state == .ended {
+            
+        }
+        
+    }
+    
+    
+    
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
