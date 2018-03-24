db7bdc54dcbdb4fe48c14236e7ca97edc1e93c02
Alessia Masola <2202343m@student.gla.ac.uk>
2018-03-24 10:56:12 -0700

    Initial Commit

---
+++ HomeSafe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeSafe
+//
+//  Created by Alessia Masola on 24/03/2018.
+//  Copyright © 2018 Alessia Masola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
